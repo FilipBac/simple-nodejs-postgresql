ae95ee588887d0e6350e6c32a74ef744ca88d6c5
Filip Bacinger <filip.bacinger@farseer.io>
2025-06-24 11:05:42 +0200

    feat: added /health endpoint

--- server/index.js
+++ server/index.js
@@ -5,11 +5,14 @@ import middlewaresConfig from './config/middlewares';
 const app = express();
 middlewaresConfig(app);
 
+app.get('/health', (req, res) => {
+  res.status(200).send(JSON.stringify({ health: 'OK' }));
+});
 app.use('/api', [UserRoutes, PostRoutes]);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, err => {
+app.listen(PORT, (err) => {
   if (err) {
     console.error(`👽Houston we have a problem : ${err} ☠️`);
   }
